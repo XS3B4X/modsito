e5c41fb4abc75f1dd5edf7b085db7989443692f6
XS3B4X <sebastian2003.saez@gmail.com>
2025-02-06 21:24:40 -0300

    Inicio Modsito Tierra aria

--- Items/Accessories/Damage/ReinforcedBowString.cs
+++ Items/Accessories/Damage/ReinforcedBowString.cs
@@ -2,26 +2,73 @@ using Terraria;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Modsito.Items.Accessories.Damage
 {
     internal class ReinforcedBowString : ModItem
     {
-        public static readonly int AdditiveDamageBonus = 10;
-        public static readonly int AttackSpeedBonus = 20;
-
-        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(AdditiveDamageBonus, AttackSpeedBonus);
+        public static readonly int additiveDamageBonus = 10;
+        public static readonly int attackSpeedBonus = 20;
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(additiveDamageBonus, attackSpeedBonus);
         public override void SetDefaults()
         {
             Item.width = 18;
             Item.height = 18;
             Item.accessory = true;
             Item.rare = ItemRarityID.Blue;
+            Item.value = Item.buyPrice(silver: 37, copper: 85);
         }
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.GetDamage(DamageClass.Ranged) += AdditiveDamageBonus / 100f;
-            player.GetAttackSpeed(DamageClass.Ranged) += AttackSpeedBonus / 100f;
+            player.GetModPlayer<BowDamagePlayer>().HasReinforcedBowString = true;
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe1 = CreateRecipe();
+            recipe1.AddIngredient(ItemID.Cobweb, 15);
+            recipe1.AddTile(TileID.Loom);
+            recipe1.Register();
+        }
+    }
+    public class BowDamagePlayer : ModPlayer
+    {
+        public bool HasReinforcedBowString = false;
+        public override void ResetEffects()
+        {
+            HasReinforcedBowString = false;
+        }
+        public override void ModifyWeaponDamage(Item item, ref StatModifier damage)
+        {
+            if (HasReinforcedBowString && item.useAmmo == AmmoID.Arrow)
+            {
+                damage *= 1.10f; // +10% daño solo para arcos
+            }
+        }
+        public override void PostUpdate()
+        {
+            if (HasReinforcedBowString && Main.LocalPlayer.HeldItem.useAmmo == AmmoID.Arrow)
+            {
+                Item heldItem = Main.LocalPlayer.HeldItem;
+
+                if (!heldItem.TryGetGlobalItem(out BowGlobalItem bowStats))
+                {
+                    heldItem.GetGlobalItem<BowGlobalItem>().ApplyModifiers(heldItem);
+                }
+            }
+        }
+    }
+    public class BowGlobalItem : GlobalItem
+    {
+        private bool modified = false;
+        public void ApplyModifiers(Item item)
+        {
+            if (!modified && item.useAmmo == AmmoID.Arrow)
+            {
+                item.useTime = (int)(item.useTime * 0.80f); // Reduce un 20% el tiempo de uso
+                item.useAnimation = (int)(item.useAnimation * 0.80f);
+                modified = true;
+            }
         }
     }
 }
---
+++ Items/Weapons/Ranger/SawThrower/ChlorophiteSawThrower.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Modsito.Items.Weapons.Ranger.Ammo.Saws;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Modsito.Projectiles.Ammo.Saws;
+
+namespace Modsito.Items.Weapons.Ranger.SawThrower
+{
+    public class ClorophiteSawThrower : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DamageType = DamageClass.Ranged;
+            Item.noMelee = true;
+            Item.knockBack = 2.5f;
+            Item.damage = 70;
+            Item.width = 21;
+            Item.height = 21;
+            Item.useAnimation = 20;
+            Item.useTime = 8;
+            Item.reuseDelay = 30;
+            Item.consumeAmmoOnFirstShotOnly = true;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.shootSpeed = 26f;
+            Item.UseSound = SoundID.Item1;
+            Item.value = 7250;
+            Item.rare = ItemRarityID.Pink;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<SawBladeProjectile>();
+            Item.useAmmo = ModContent.ItemType<SawBlade>();
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe1 = CreateRecipe();
+            recipe1.AddIngredient(ItemID.ChlorophyteBar, 15);
+            recipe1.AddTile(TileID.MythrilAnvil);
+            recipe1.Register();
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-1f, 1f);
+        }
+    }
+}
